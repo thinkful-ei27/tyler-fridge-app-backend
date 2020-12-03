a96bbacb92c124d7bd96109c3ec2e8f98116f2e1
Tyler Crabb <tydacrabb@gmail.com>
2020-12-02 18:10:58 -0800

    fixing mongodb atlas

--- utils/seed.js
+++ utils/seed.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-const  MONGODB_URI  = 'mongodb + srv://Tcrabb:thedarktower19@cluster0.hhxta.mongodb.net/fridge?retryWrites=true&w=majority';
+// const MONGODB_URI =
 
 const FoodItem = require('../models/foodItem');
 
